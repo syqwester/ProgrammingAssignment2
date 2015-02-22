806cc73ee562f6ba18eee9e2f2ae9b873f7c71e6
Syqwester <lucas@sybyt.com>
2015-02-22 13:20:47 -0500

    Assignment 2
    Program assignment 2 for Coursera R course

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+##  cachematrix is a function containing two independent functions 
+##  that cache the inverse of a matrix.
 
-## Write a short comment describing this function
+##  makeCacheMatrix creates a object containing a special "matrix" 
+##  capable of being cached as its inverse.
+##  It uses a list of functions to create the special "matrix" object
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL                       # Sets inverse variable to Null
+  set <- function(y) {              # Sets default values to cache
+    x <<- y                         # Cache is stored in new environment
+    inv <<- NULL
+  }
+  
+  get <- function() x                           #  Retrieves original matrix
+  setinverse <- function(flip) inv <<- flip     #  Saves a new inversed matrix
+  getinverse <- function() inv                  #  Retrieves inversed matrix
+  list(set = set, get = get,                    #  Creates list of functions
+       setinverse = setinverse,                 #  to return
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+##  cacheSolve is a function that computes the inverse using a special "matrix" 
+##  returned by makeCacheMatrix if it hasn't been calculated previously, 
+##  otherwise it returns cached inverted matrix
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...){
+
+## Returns a matrix that is the inverse of 'x' if none exists or the cached 
+  ## inversed matrix if it has already been caluclated
+  
+  inv <- x$getinverse()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setinverse(inv)
+  inv
 }
